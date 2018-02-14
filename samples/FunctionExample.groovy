9b9f981dd5529a33a3445b2bfbad7d296b912b8f
Antoine AUBÉ <aube.antoine@protonmail.com>
2018-02-14 20:09:17 +0000

    Added new samples

---
+++ samples/FunctionExample.groovy
@@ -0,0 +1,17 @@
+laws {
+    law 'occupancy' is function(0..24) {
+        when 0..1 then {x ** 2}
+        when 1..5 then {1 / x}
+        otherwise {x - 1}
+    }
+}
+
+stimulation {
+    lot ('School') {
+        contains 12 sensors 'place' following 'occupancy' parameterized {
+            during 1.year sampleEvery 1.second
+
+            period 3.hours
+        }
+    }
+}
---
+++ samples/InterpolateExample.groovy
@@ -0,0 +1,18 @@
+laws {
+    law 'occupancy' is interpolate(0..24) {
+        x 6     y 0
+        x 8     y 75
+        x 9     y 100
+        x 13    y 75
+    }
+}
+
+stimulation {
+    lot ('School') {
+        contains 12 sensors 'place' following 'occupancy' parameterized {
+            during 1.year sampleEvery 1.second
+
+            period 3.hours
+        }
+    }
+}
